253d662150f2fd6085f43b8bb9e8ab51e85d3271
vpapenko <vpapenko@gmail.com>
2020-09-29 14:14:25 +0300

    Initial commit

---
+++ ColorPicker.Nuget.Test/ColorPicker.Nuget.Test/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ColorPicker.Nuget.Test
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SKCanvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            SKCanvas canvas = e.Surface.Canvas;
+
+            var scale = 21F;
+            SKPath path = new SKPath();
+            path.MoveTo(-1 * scale, -1 * scale);
+            path.LineTo(0 * scale, -1 * scale);
+            path.LineTo(0 * scale, 0 * scale);
+            path.LineTo(1 * scale, 0 * scale);
+            path.LineTo(1 * scale, 1 * scale);
+            path.LineTo(0 * scale, 1 * scale);
+            path.LineTo(0 * scale, 0 * scale);
+            path.LineTo(-1 * scale, 0 * scale);
+            path.LineTo(-1 * scale, -1 * scale);
+
+            SKMatrix matrix = SKMatrix.MakeScale(2 * scale, 2 * scale);
+            SKPaint paint = new SKPaint
+            {
+                PathEffect = SKPathEffect.Create2DPath(matrix, path),
+                Color = Color.LightGray.ToSKColor(),
+                IsAntialias = true
+            };
+
+            var patternRect = new SKRect(0, 0, ((SKCanvasView)sender).CanvasSize.Width, ((SKCanvasView)sender).CanvasSize.Height);
+
+            canvas.Save();
+            canvas.DrawRect(patternRect, paint);
+            canvas.Restore();
+        }
+    }
+}
